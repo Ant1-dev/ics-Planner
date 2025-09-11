7240abc5d8fb7796b072f0843e22fb169a8ba4ad
Ant1 <47747877+Ant1-dev@users.noreply.github.com>
2025-09-11 17:07:35 -0400

    Project inialization

---
+++ src/main/java/com/toine/icsPlanner/IcsPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.toine.icsPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcsPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcsPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toine/icsPlanner/IcsPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toine.icsPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcsPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
